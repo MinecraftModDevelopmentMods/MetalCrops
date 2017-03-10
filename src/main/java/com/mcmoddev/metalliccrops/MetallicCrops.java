6011fc0142c4e7b82cb6fcab0f99b1d5d13a67f6
knoxhack <hackofgod@gmail.com>
2017-03-10 12:11:38 -0500

    update version

--- src/main/java/com/mcmoddev/metalliccrops/MetallicCrops.java
+++ src/main/java/com/mcmoddev/metalliccrops/MetallicCrops.java
@@ -12,7 +12,7 @@ public class MetallicCrops
 {
     public static final String MODID = "metalliccrops";
     public static final String MODNAME = "Metallic Crops";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     
     @SidedProxy(clientSide = "com.mcmoddev.metalliccrops.proxy.ClientProxy", serverSide = "com.mcmoddev.metalliccrops.proxy.CommonProxy")
     public static CommonProxy proxy;
